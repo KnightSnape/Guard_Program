0e69a3d8d094d885548809d16e701bff40119662
KnightSnape <245276732@qq.com>
2023-06-02 15:09:00 +0800

    fix: rebuild navigation

--- navigation_node/src/navigation_node.cpp
+++ navigation_node/src/navigation_node.cpp
@@ -1,9 +1,26 @@
-#include"navigation.hpp"
+#include<string>
+#include<ros/ros.h>
+#include<ros/package.h>
+#include<Eigen/Eigen>
+#include<yaml-cpp/yaml.h>
+#include"std_msgs/Bool.h"
+#include"nav_msgs/Odometry.h"
+#include"geometry_msgs/PointStamped.h"
+#include"geometry_msgs/TwistStamped.h"
+#include<memory>
+#include<thread>
+#include<mutex>
+
+void odometry_callback(const nav_msgs::Odometry::ConstPtr msg)
+{
+
+}
 
 int main(int argc,char **argv)
 {
-    ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug);
     ros::init(argc,argv,"navigation_node");
-    navigation nav;
+    ros::NodeHandle nh;
+    ros::Subscriber odom_sub = nh.subscribe<nav_msgs::Odometry>("/Odometry",100000,odometry_callback);
+    ros::spin();
     return 0;
 }
